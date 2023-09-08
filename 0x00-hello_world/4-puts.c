db766e70fbf1739a9a97f8e6c575219bab99f8d4
AishaMahjoub <asha.mahjoub@gmail.com>
2023-09-08 12:20:54 -0700

    task0for c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 /**
- * this the maim function 
- * Return 0
+ * main - Entry point
+ *
+ * Description: c program that print put function 
+ *
+ * return :always 0 (success)
+ 
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\ "Programming is like building a multilingual puzzl");
+
+	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/** 
+ * main - Entary point
+ *
+ * Description : A c program that print with printf function 
+ *
+ * Return : alyaws 0 (succed)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
